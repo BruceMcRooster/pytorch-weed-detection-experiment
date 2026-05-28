52a4aaf2229d53dbb4696c6ed2ebc91d1387d6c3
Evan Foster <evanfost@andrew.cmu.edu>
2026-05-28 16:00:24 -0400

    add prediction script

--- main.py
+++ main.py
@@ -40,6 +40,36 @@ def train(
 
     return model, results
 
+def predict(
+    weights: Path,
+    input_file: Path,
+    conf: float = 0.5,
+):
+    if type(weights) != Path:
+        weights = Path(weights)
+    if type(input_file) != Path:
+        input_file = Path(input_file)
+
+    assert weights.is_file() and (weights.suffix.lower() == '.pt' or weights.suffix.lower() == '.pth'), \
+        "weights must be a pytorch model weight save file"
+
+    model = YOLO(weights)
+
+    results = {}
+
+    if input_file.is_dir():
+        print(f"Detected input_file is a directory, iterating over files")
+
+    # deduplicate logic to keep it in sync
+    for file in input_file.iterdir() if input_file.is_dir() else [input_file]:
+        if file.suffix != '.jpg' and file.suffix != '.jpeg' and file.suffix != '.png':
+            print(f"Skipping {file}")
+            continue
+        result = model(file, save=True, conf=conf)
+        results[file] = result
+
+    return results
+
 if __name__ == "__main__":
     import argparse
 
@@ -47,12 +77,22 @@ if __name__ == "__main__":
     subparsers = parser.add_subparsers(dest='command', help='Available commands', required=True)
 
     add_subparser(subparsers, train, 'train')
+    add_subparser(subparsers, predict, 'predict')
 
     args = vars(parser.parse_args())
 
     target_function = args.pop('func')
     args.pop('command')
 
-    target_function(**args)
+    if target_function == train:
+        model, results = target_function(**args)
+        print("\n\n=====FINAL RESULTS=====\n\n")
+        print(results)
+    elif target_function == predict:
+        results = target_function(**args)
+        print("\n\n=====PREDICTION RESULTS=====\n\n")
+        for file, result in results.items():
+            print(f"\n====={file}=====\n")
+            print(result)
 
 
